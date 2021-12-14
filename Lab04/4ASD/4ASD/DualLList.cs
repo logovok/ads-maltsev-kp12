5eda37375b8db8e48820429b245c8f46d3f19e2d
logovok <logovok093@gmail.com>
2021-12-14 16:44:26 +0300

    Add files via upload

---
+++ Lab04/4ASD/4ASD/DualLList.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4ASD
+{
+    class DualLList
+    {
+        public void dsplay() {
+            DLNode temp = head;
+            int c = 0;
+            while (true)
+            {
+                Console.WriteLine("№ " + c + " " + temp.data);
+                ++c;
+                if (temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                else
+                {
+                    break;
+                }
+                
+
+            } 
+        }
+        public DLNode head;
+        public class DLNode {
+            public DLNode prev;
+            public int data;
+            public DLNode next;
+            
+            public DLNode(DLNode pr, int dt, DLNode nxt) {
+                prev = pr;
+                data = dt;
+                next = nxt;
+            }
+            public DLNode(int dt, DLNode nxt)
+            { 
+                data = dt;
+                next = nxt;
+            }
+            public DLNode(DLNode pr, int dt)
+            {
+                prev = pr;
+                data = dt;
+            }
+            public DLNode(int dt)
+            {
+                data = dt;
+            }
+        }
+
+        public DualLList(DLNode headOflist) {
+            head = headOflist;
+        }
+
+        public void AddFirst(int data) {
+            DLNode temp = new DLNode(data,head);
+            head.prev = temp;                    
+            head = temp;   
+        }
+
+        public void DelFirst() {
+            head = head.next;
+            head.prev = null;
+        }
+         
+
+        public void Add2Pos(int pos, int dat)
+        {
+            
+            DLNode temp = head;
+            while (true)
+            {
+                if (pos == 0)
+                {
+                    DLNode t2 = new DLNode(temp.prev,dat,temp);
+                    temp.prev.next = t2;
+                    temp.prev = t2;
+                    
+                    break;
+                }
+                if (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                else
+                {
+                    break;
+                }
+                pos--;
+
+            }
+        }
+
+        public void Del2Pos(int pos)
+        {
+
+            DLNode temp = head;
+            while (true)
+            {
+                if (pos == 0)
+                {
+                    temp.prev.next = temp.next;
+                    temp.next.prev = temp.prev;
+
+                    break;
+                }
+                if (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                else
+                {
+                    break;
+                }
+                pos--;
+
+            }
+        }
+
+        public void AddLust(int dta)
+        {
+            DLNode temp = head;
+            while (true)
+            {
+
+                if (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                else
+                {
+                    temp.next = new DLNode(temp, dta);
+
+                    break;
+                }
+
+            }
+        }
+
+        public void DelLust()
+        {
+            DLNode temp = head;
+            while (true)
+            {
+                if (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                else
+                {
+                    temp.prev.next = null;
+                    break;
+                }
+
+            }
+        }
+
+        
+    }
+}
---
+++ Lab04/4ASD/4ASD/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace _4ASD
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first element");
+            DualLList dll = new DualLList(new DualLList.DLNode(int.Parse(Console.ReadLine())));
+            void Vari3Zavd(int k)
+            {
+                if (k > 0 && k < 99)
+                {
+                    dll.Add2Pos(1, k);
+                }
+                else
+                {
+                    dll.AddFirst(k);
+                }
+            }
+            bool g = true;
+            while (g)
+            {
+                string temp = Console.ReadLine();
+                try
+                {
+
+
+                    switch (temp)
+                    {
+                        case "stop":
+                            g = false;
+                            break;
+                        case "help":
+                            helpMSG();
+                            break;
+                        case "AddFirst":
+                            Console.WriteLine("Enter your value");
+                            dll.AddFirst(int.Parse(Console.ReadLine()));
+                            dll.dsplay();
+                            Console.WriteLine("Enter next command please");
+                            break;
+                        case "DeleteFirst":
+                            dll.DelFirst();
+                            dll.dsplay();
+                            Console.WriteLine("Enter next command please");
+                            break;
+                        case "Add2Position":
+                            Console.WriteLine("Enter posoiton");
+                            int i = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter value");
+                            dll.Add2Pos(i,int.Parse(Console.ReadLine()));
+                            dll.dsplay();
+                            Console.WriteLine("Enter next command please");
+                            break;
+                        case "DeleteAtPos":
+                            Console.WriteLine("Enter position");
+                            dll.Del2Pos(int.Parse(Console.ReadLine()));
+                            dll.dsplay();
+                            Console.WriteLine("Enter next command please");
+                            break;
+                        case "AddLast":
+                            Console.WriteLine("Enter value");
+                            dll.AddLust(int.Parse(Console.ReadLine()));
+                            dll.dsplay();
+                            Console.WriteLine("Enter next command please");
+                            break;
+                        case "DeleteLast":
+                            dll.DelLust();
+                            dll.dsplay();
+                            Console.WriteLine("Enter next command please");
+                            break;
+                        case "Print":
+                            dll.dsplay();
+                            Console.WriteLine("Enter next command please");
+                            break;
+
+                        default:
+                            Console.WriteLine();
+                            Console.WriteLine("Wrong command, pleas read healp message!");
+                            helpMSG();
+                            break;
+                    }
+                    
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Command cannot be performed");
+                    Console.WriteLine("Type of elements is integer!");
+                    Console.WriteLine("try to use another one");
+                    helpMSG();
+                    Console.WriteLine("=====");
+                    Console.WriteLine("Try command: Print");
+                    Console.WriteLine("If it couldnt be performed you may have deleted all elements");
+                    Console.WriteLine("In that case program needs restart");
+                }
+            }
+            
+        }
+        static void helpMSG() {
+            Console.WriteLine("To stop the program type: stop");
+            Console.WriteLine("To add element in head type: AddFirst");
+            Console.WriteLine("To delete element from head type: DeleteFirst");
+            Console.WriteLine("To add element in position type: Add2Position");
+            Console.WriteLine("To delete element frim position type: DeleteAtPos");
+            Console.WriteLine("To add element to last posotion type : AddLast");
+            Console.WriteLine("To delete element from last posotion type : DeleteLast");
+            Console.WriteLine("To see all elements type: Print");
+        }
+        
+    }
+}
